222519e0af25ad829be2e83fdf316a85150d73f1
Mike Tase <tasemgt@yahoo.com>
2016-08-06 11:44:09 +0000

    Testing some shit!

--- listProducts.js
+++ listProducts.js
@@ -3,6 +3,6 @@ var Faker = require("Faker");
 for(var i =0; i<10; i++){
   console.log(Faker.Name.firstName(), String(Faker.random.number())); 
   var hello = function(){
-    
+    console.log("This is my world!!");
   }
 }
